78557dfdde16c3f43fe620e624ee6edf491bb32b
RkzNew <rahamannovel@gmail.com>
2022-10-11 02:09:15 +0600

    Added toast in correct incorrect amswer

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Question.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Question = ({questionQuiz, count}) => {
+    
+
     const {question, options,correctAnswer} = questionQuiz;
     
     const correctHandler = (answer) => {
         if(answer === correctAnswer){
-            
+            toast.success('Correct Answer', { position: "top-center" })
+        }
+        else{
+            toast.warn('Wrong Answer', { position: "top-center" })
         }
     }
 
@@ -13,7 +21,7 @@ const Question = ({questionQuiz, count}) => {
         <div className='quiz-question-container'>
             <h3><strong>Question: {count}</strong> {} {question}</h3>
             <div className='options-container'>
-              {options.map(answer => <div className='answer-container' key={answer} onClick={() => correctHandler(answer)}> {answer} </div>)}
+              {options.map(answer => <div className='answer-container' key={answer} onClick={() => correctHandler(answer)} > {answer} </div>)}<ToastContainer />
             </div>
         </div>
     );
