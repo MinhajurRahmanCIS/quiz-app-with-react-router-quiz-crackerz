10a28211ae52a9a179804124b7d7968b4ca49ada
RkzNew <rahamannovel@gmail.com>
2022-10-11 00:37:10 +0600

    displaye qustions and answers

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-
+import './Topic.css'
 const Topic = () => {
     const quizQuestion = useLoaderData();
     const options = quizQuestion.data.questions;
     console.log(options)
     return (
-        <div>
+        <div className='topic-container'>
+            <h1>Quiz of {quizQuestion.data.name}</h1>
+            <p>Total <strong>{options.length}</strong>   question</p>
           {
-           options.map(question => <div><h3>{question.question}</h3></div> )
+           options.map(question => <div className='question-container'><h3>{question.question}</h3>
+           <div className='quizQuestion-container'>
+            <div className="quizQuestion">{question.options[0]}</div>
+            <div className="quizQuestion">{question.options[1]}</div>
+            <div className="quizQuestion">{question.options[2]}</div>
+            <div className="quizQuestion">{question.options[3]}</div>
+            
+           </div>
+           </div> )
           }
         </div>
     );
